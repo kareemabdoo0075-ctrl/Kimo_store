b19eade9b69c0f53e5cf604de32beb011f5f9850
kareemabdoo0075-ctrl <kareemabdoo0075@gmail.com>
2026-09-08 19:50:08 +0300

    Update games.js

--- games.js
+++ games.js
@@ -1,1 +1,45 @@
+const games = [
+    // 1. ألعاب أونلاين
+    { 
+        title: "قتال العصي الملحمي", 
+        category: "online", 
+        img: "https://img.gamedistribution.com/rvvAS308-512x512.jpeg", 
+        url: "https://html5.gamedistribution.com/rvvAS308/",
+        type: "play"
+    },
+    { 
+        title: "سباق السيارات Nitro", 
+        category: "online", 
+        img: "https://img.gamedistribution.com/62497fb26a114f04a601e3b6107383da-512x512.jpeg", 
+        url: "https://html5.gamedistribution.com/62497fb26a114f04a601e3b6107383da/",
+        type: "play"
+    },
+
+    // 2. ألعاب أوفلاين
+    { 
+        title: "قص الحبل Cut The Rope", 
+        category: "offline", 
+        img: "https://img.gamepix.com/games/cut-the-rope/icon/cut-the-rope.png", 
+        url: "https://play.gamepix.com/cut-the-rope/embed",
+        type: "play"
+    },
+
+    // 3. ألعاب للتحميل
+    { 
+        title: "PUBG Mobile", 
+        category: "download", 
+        img: "https://play-lh.googleusercontent.com/JRd0B3A399-4w2H6BNDkd8n2334863_100", 
+        url: "https://play.google.com/store/apps/details?id=com.tencent.ig",
+        type: "download"
+    },
+
+    // 4. الأكثر شعبية
+    { 
+        title: "بطل القتال Brawl Hero", 
+        category: "popular", 
+        img: "https://img.gamedistribution.com/f0426189db894819a16a8d80c058c49e-512x512.jpeg", 
+        url: "https://html5.gamedistribution.com/f0426189db894819a16a8d80c058c49e/",
+        type: "play"
+    }
+];
 
